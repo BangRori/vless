e7ce7e7c407feded1e4eb15a1db2b1a942c66700
vivask <vivask@mail.ru>
2026-04-08 09:38:24 +0300

    first commit

---
+++ dpi-alert/tcpdump-tls.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PCAP_DIR="/var/log/tcpdump"
+MAX_AGE_MIN=30  # удалять файлы старше 30 минут
+
+# Чистим старые файлы перед запуском
+find "$PCAP_DIR" -name "tls-*.pcap" -mmin +$MAX_AGE_MIN -delete
+
+exec /usr/sbin/tcpdump \
+    -i ens3 \
+    -Z root \
+    -w "$PCAP_DIR/tls-%s.pcap" \
+    -G 300 \
+    -n \
+    'tcp port 443 and tcp[tcpflags] & tcp-syn != 0'
